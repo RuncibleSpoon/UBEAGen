6d48e4a7e833d5f41292eded6d4fd8f5076e6d8a
RuncibleSpoon <robert.haynes@gmail.com>
2020-05-15 11:25:41 -0700

    Update GenerateS3.py

--- GenerateS3.py
+++ GenerateS3.py
@@ -26,18 +26,19 @@ def create_bucket(bucket_name, region=None):
         else:
             s3_client = boto3.client('s3', region_name=region)
             location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
+            mybucket = s3_client.create_bucket(Bucket=bucket_name,
                                     CreateBucketConfiguration=location)
     except ClientError as e:
         logging.error(e)
         return False
-    return True
+    return mybucket.
 
 def main(argv=None):
     print("Starting %s" % time.ctime())
 
-    bucketno = random.randrange(1000)
-    bucketName = "AnomB" + bucketno
+    bucketno = str(random.randrange(1000))
+    bucketName = "anombkt7883" + bucketno
+    print("Bucket: ", bucketName)
     region = "us-west-2"
 
     s3B = create_bucket(bucketName,region)
